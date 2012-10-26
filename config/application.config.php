1f510eef8e24d0b14185e6c13d3d2aeed1ede790
Westin Shafer <wshafer@relivinc.com>
2012-10-25 20:36:26 -0700

    Removed Doctrine Submodules.  Seems to not work unless installed via composer

--- config/application.config.php
+++ config/application.config.php
@@ -1,15 +1,38 @@
 <?php
 return array(
     'modules' => array(
+
         'Application',
+
+        //Rcm Dependencies
+        'DoctrineModule',
+        'DoctrineORMModule',
+
+        //RCM core and plugins
+        'Rcm',
+        'RcmJsonDataPluginToolkit',
+        'RcmHtmlArea',
+        'RcmNavigation',
+        'RcmCallToActionBox',
+        'RcmPortalAnnouncementBox',
+        'RcmLogin',
+        'RcmSocialButtons',
+        'RcmRssFeed',
+
+        //MUST BE AT BOTTOM OF DEFINITION
+        'ElFinder',
     ),
     'module_listener_options' => array(
-        'config_glob_paths'    => array(
+        'config_glob_paths' => array(
             'config/autoload/{,*.}{global,local}.php',
         ),
         'module_paths' => array(
             './module',
             './vendor',
+            './vendor/reliv',
+            './vendor/reliv/RcmPlugins',
+            './vendor/doctrine/doctrine-module/src',
+            './vendor/doctrine/doctrine-orm-module/src',
         ),
     ),
 );
